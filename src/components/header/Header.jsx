c76e1e83e2fd1e6d65b7eb6195aa9f78955fa910
lapc18 <lapc18@outlook.com>
2022-03-26 17:54:44 -0400

    feat: add initial header design

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 import './header.scss'
+import { BsGithub, BsInstagram, BsWhatsapp } from 'react-icons/bs';
+import ME from '../../assets/me.jpg'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container container__header">
+        <h5>Welcome!</h5>
+        <h1>This is Adolfo</h1>
+        <h5 className='text-light'>Software Developer</h5>
+        <div className="actions">
+          <a className='btn' href="#">Download CV</a>
+          <a className='btn primary' href="#contact">Let's talk</a>
+        </div>
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
+        <div className="socials">
+          <a href="#" target="_blank" rel="noopener noreferrer"><BsGithub /></a>
+          <a href="#" target="_blank" rel="noopener noreferrer"><BsInstagram /></a>
+          <a href="#" target="_blank" rel="noopener noreferrer"><BsWhatsapp /></a>
+        </div>
+        <div className="scroll__down">
+          <h4>Scroll down</h4>
+        </div>
+      </div>
+    </header>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,11 +2,25 @@ import React from 'react'
 import './home.scss'
 
 import Header from '../../components/header'
+import Nav from '../../components/nav'
+import About from '../../components/about'
+import Experience from '../../components/experience'
+import MyServices from '../../components/my-services'
+import Projects from '../../components/projects'
+import Contacts from '../../components/contacts'
+import Footer from '../../components/footer'
 
 export const Home = () => {
   return (
     <>
         <Header />
+        <Nav />
+        <About />
+        <Experience />
+        <MyServices />
+        <Projects />
+        <Contacts />
+        <Footer />
     </>
   )
 }
