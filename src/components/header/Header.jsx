ef3777e01ffb3bc2b734d91da837893347817404
lapc18 <lapc18@outlook.com>
2022-03-31 16:43:30 -0400

    feat: add contact links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -19,9 +19,9 @@ export const Header = () => {
           <img src={ME} alt="" />
         </div>
         <div className="socials">
-          <a href="#" target="_blank" rel="noopener noreferrer"><BsGithub /></a>
-          <a href="#" target="_blank" rel="noopener noreferrer"><BsInstagram /></a>
-          <a href="#" target="_blank" rel="noopener noreferrer"><BsWhatsapp /></a>
+          <a href="https://github.com/lapc18" target="_blank" rel="noopener noreferrer"><BsGithub /></a>
+          <a href="https://instagram.com/luispimentel.c" target="_blank" rel="noopener noreferrer"><BsInstagram /></a>
+          <a href="https://api.whatsapp.com/send/?phone=18293972288" target="_blank" rel="noopener noreferrer"><BsWhatsapp /></a>
         </div>
         <div className="scroll__down">
           <h4>Scroll down</h4>
