50e4a6217259068e9e64b5e391370a78cd172160
lapc18 <lapc18@outlook.com>
2022-04-05 01:02:59 -0400

    feat: add initial my services design

--- src/components/my-services/MyServices.jsx
+++ src/components/my-services/MyServices.jsx
@@ -1,5 +1,36 @@
 import React from 'react'
 import './my-services.scss'
+import { ServiceBlock } from './ServiceBlock'
+import { FaLaptopCode } from 'react-icons/fa'
+import { nanoid } from '@reduxjs/toolkit'
+
+
+const services = [
+  {
+    id: nanoid(),
+    icon: <FaLaptopCode />,
+    title: 'Web Development',
+    description: 'Better perfomance, faster technologies and responsive design applied in every solution.'
+  },
+  {
+    id: nanoid(),
+    icon: <FaLaptopCode />,
+    title: 'Web Development',
+    description: 'Better perfomance, faster technologies and responsive design applied in every solution.'
+  },
+  {
+    id: nanoid(),
+    icon: <FaLaptopCode />,
+    title: 'Web Development',
+    description: 'Better perfomance, faster technologies and responsive design applied in every solution.'
+  },
+  {
+    id: nanoid(),
+    icon: <FaLaptopCode />,
+    title: 'Web Development',
+    description: 'Better perfomance, faster technologies and responsive design applied in every solution.'
+  },
+]
 
 export const MyServices = () => {
   return (
@@ -7,7 +38,11 @@ export const MyServices = () => {
       <h5>Develop all you need with</h5>
       <h2>My Services</h2>
       <div className="container container__services">
-        
+        { services.map(x => (
+          <div className='container__services__item' key={x.id}>
+            <ServiceBlock  {...x} />
+          </div>
+        )) }
       </div>
     </section>
   )
--- src/components/my-services/ServiceBlock.jsx
+++ src/components/my-services/ServiceBlock.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import './my-services.scss'
 
 export const ServiceBlock = (props) => {
   return (
-    <article className=''>
+    <article className='service-block'>
+        <div className="service-block__icon">
+            { props?.icon || '' }
+        </div>
         <div className="service-block__header">
-            <h3></h3>
+            <h3>{ props?.title || '' }</h3>
+        </div>
+        <div className="service-block__content">
+            <p>{ props?.description || '' }</p>
         </div>
     </article>
   )
